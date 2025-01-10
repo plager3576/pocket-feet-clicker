6fca2529debbf17cfb677b677e3c6f1968ee5c92
Szymon <amikusek30@gmail.com>
2025-01-10 23:21:52 +0100

    STOPYYYY

---
+++ clicker.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var stopy = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_stupka_pressed() -> void:
+	stopy += 1
+	print(stopy)
